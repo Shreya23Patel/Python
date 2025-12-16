c38e66cc3823b6d5746e74431bc232db1f7b2d7a
Shreya23Patel <159240845+Shreya23Patel@users.noreply.github.com>
2025-12-16 11:09:18 +0530

    Delete Python directory

--- Python/11vovel.py
+++
@@ -1,7 +0,0 @@
-
-a = input("Enter a letter: ")
-
-if a.lower() in ['a', 'e', 'i', 'o', 'u']:
-    print(a, "is a vowel")
-else:
-    print(a, "is not a vowel")
--- Python/6fibonccai.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the range: "))
-
-a, b = 0, 1
-
-print("Fibonacci Series:")
-for i in range(n):
-    print(a, end=" ")
-    a, b = b, a + b
-    
